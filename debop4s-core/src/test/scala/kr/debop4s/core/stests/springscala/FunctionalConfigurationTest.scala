994e07736d6fe6343d82ead96ecbcf38d2a74130
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2014-01-12 03:41:34 +0900

    -. Spring-Scala 로 환경설정하는 것을 구현했지만, 굳이 도입할 필요가 없을 것 같음.

---
+++ debop4s-core/src/test/scala/kr/debop4s/core/stests/springscala/FunctionalConfigurationTest.scala
@@ -0,0 +1,37 @@
+package kr.debop4s.core.stests.springscala
+
+import kr.debop4s.core.compress.{DeflateCompressor, GZipCompressor}
+import kr.debop4s.core.logging.Logger
+import org.scalatest.FunSuite
+import org.springframework.scala.context.function.{FunctionalConfigApplicationContext, FunctionalConfiguration}
+
+/**
+ * Spring-Scala 를 이용하여 Spring Configuration을 수행했지만, 좋아보이지는 않는다.
+ * 기존 방식이 크게 어려움이 없기 때문에 그냥 사용하기로 했다.
+ * @author 배성혁 sunghyouk.bae@gmail.com
+ * @since  2014. 1. 12. 오전 3:19
+ */
+class FunctionalConfigurationTest extends FunSuite {
+    lazy val log = Logger[FunctionalConfigurationTest]
+
+    val context = FunctionalConfigApplicationContext(classOf[CompressorConfiguration])
+    val gzip = context.getBean(classOf[GZipCompressor])
+    val deflate = context.getBean(classOf[DeflateCompressor])
+    test("compressor injection") {
+        log.debug("names=" + context.getBeanDefinitionNames.mkString(","))
+
+        assert(gzip != null)
+        assert(deflate != null)
+    }
+}
+
+class CompressorConfiguration extends FunctionalConfiguration {
+
+    bean() {
+        new GZipCompressor()
+    }
+
+    bean() {
+        new DeflateCompressor()
+    }
+}
