5bcf1e79627a16618794f9b9f0fb92ed078d30c6
배성혁 <sunghyouk.bae@gmail.com>
2014-03-04 22:40:50 +0900

    -. Hikari CP 사용 테스트 추가.

--- debop4s-data/src/main/scala/com/github/debop4s/data/jdbc/DataSources.scala
+++ debop4s-data/src/main/scala/com/github/debop4s/data/jdbc/DataSources.scala
@@ -5,6 +5,7 @@ import com.zaxxer.hikari.{HikariDataSource, HikariConfig}
 import java.util.Properties
 import javax.sql.DataSource
 import org.slf4j.LoggerFactory
+import scala.collection.immutable.HashMap
 
 /**
  * [[javax.sql.DataSource]] 를 생성, 제공하는 Object 입니다.
@@ -26,26 +27,15 @@ object DataSources {
    *
    * @param driverClass DriverClass 명
    * @param url         Database 주소
-   * @return [[javax.sql.DataSource]] 인스턴스
-   */
-  def getDataSource(driverClass: String, url: String): DataSource =
-    getDataSource(driverClass, url, "", "")
-
-
-  /**
-   * [[javax.sql.DataSource]] 를 빌드합니다. 기본적으로 Tomcat DataSource 를 사용합니다.
-   *
-   * @param driverClass DriverClass 명
-   * @param url         Database 주소
    * @param username    사용자 명
    * @param passwd      사용자 패스워드
    * @return [[javax.sql.DataSource]] 인스턴스
    */
-  def getDataSource(driverClass: String, url: String, username: String, passwd: String): DataSource = {
+  def getDataSource(driverClass: String, url: String, username: String = "", passwd: String = ""): DataSource = {
     getBoneCPDataSource(driverClass, url, username, passwd)
   }
 
-  def getBoneCPDataSource(driverClass: String, url: String, username: String, passwd: String): DataSource = {
+  def getBoneCPDataSource(driverClass: String, url: String, username: String = "", passwd: String = ""): DataSource = {
     log.info("BoneCP DataSource를 빌드합니다... " +
              s"driverClass=[$driverClass], url=[$url], username=[$username], passwd=[$passwd]")
 
@@ -70,7 +60,11 @@ object DataSources {
    * @param passwd      사용자 패스워드
    * @return [[javax.sql.DataSource]] 인스턴스
    */
-  def getHikariDataSource(dataSourceClassName: String, url: String, username: String, passwd: String = ""): DataSource = {
+  def getHikariDataSource(dataSourceClassName: String,
+                          url: String,
+                          username: String = "",
+                          passwd: String = "",
+                          props: Map[String, String] = HashMap()): DataSource = {
     log.info("Hikari DataSource를 빌드합니다... " +
              s"dataSourceClassName=[$dataSourceClassName], url=[$url], username=[$username], passwd=[$passwd]")
 
@@ -81,6 +75,10 @@ object DataSources {
     config.addDataSourceProperty("url", url)
     config.addDataSourceProperty("user", username)
     config.addDataSourceProperty("password", passwd)
+
+    props.foreach { prop =>
+      config.addDataSourceProperty(prop._1, prop._2)
+    }
 
     config.setAcquireIncrement(processCount * 2)
     config.setMaximumPoolSize(processCount * 50)
@@ -108,15 +106,19 @@ object DataSources {
   * }}}
   */
   def getHirakiDataSource(props: Properties): DataSource = {
+    log.info(s"Hikari DataSource를 빌드합니다... props=$props")
+
     val config = new HikariConfig(props)
     new HikariDataSource(config)
   }
 
   /** 테스트에 사용하기 위해 메모리를 사용하는 HSql DB 에 대한 DataSource 를 반환합니다. */
-  def getEmbeddedHSqlDataSource: DataSource =
+  def getEmbeddedHSqlDataSource: DataSource = {
     getDataSource(HSQL_DRIVER_CLASS_NAME, "jdbc:hsqldb:mem:test;MVCC=TRUE;", "sa", "")
+  }
 
   /** 테스트에 사용하기 위해 메모리를 사용하는 H2 DB 에 대한 DataSource 를 반환합니다. */
-  def getEmbeddedH2DataSource: DataSource =
+  def getEmbeddedH2DataSource: DataSource = {
     getDataSource(H2_DRIVER_CLASS_NAME, "jdbc:h2:mem:test;DB_CLOSE_ON_EXIT=FALSE;MVCC=TRUE;", "sa", "")
+  }
 }
--- debop4s-data/src/test/scala/com/github/debop4s/data/tests/jdbc/DataSourcesTest.scala
+++ debop4s-data/src/test/scala/com/github/debop4s/data/tests/jdbc/DataSourcesTest.scala
@@ -4,6 +4,7 @@ import com.github.debop4s.data._
 import com.github.debop4s.data.jdbc.DataSources
 import java.util.Properties
 import org.scalatest.{Matchers, FunSuite}
+import scala.collection.immutable.HashMap
 
 /**
  * DataSourcesTest
@@ -21,14 +22,21 @@ class DataSourcesTest extends FunSuite with Matchers {
     conn.isClosed should equal(true)
   }
   test("HikariCP create multiple datasources by promgramatic") {
-    val ds = DataSources.getHikariDataSource(DATASOURCE_CLASS_H2, "jdbc:h2:mem:test", "sa", "")
+    val ds = DataSources.getHikariDataSource(DATASOURCE_CLASS_MYSQL,
+                                              "jdbc:mysql://localhost:3306/test",
+                                              "sa",
+                                              "",
+                                              HashMap("characterEncoding" -> "UTF-8", "useUnicode" -> "true"))
     ds should not be null
 
     (0 until 100).par.foreach { x =>
       val conn = ds.getConnection
       val ps = conn.prepareStatement("SELECT 1")
       ps.execute() should equal(true)
+
+      Thread.sleep(10)
       conn.close()
+      conn.isClosed should equal(true)
     }
   }
 
@@ -44,6 +52,7 @@ class DataSourcesTest extends FunSuite with Matchers {
       val conn = ds.getConnection
       val ps = conn.prepareStatement("SELECT 1")
       ps.execute() should equal(true)
+      Thread.sleep(10)
       conn.close()
     }
   }
