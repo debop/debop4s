7d296fd1130d04bb1dae60ed4920f0fda312c08f
배성혁 <sunghyouk.bae@gmail.com>
2014-03-28 08:35:40 +0900

    -. remove gridgain

--- debop4s-experiments/src/test/scala/com/github/debop4s/experiments/tests/serializers/GridSerializerTest.scala
+++ debop4s-experiments/src/test/scala/com/github/debop4s/experiments/tests/serializers/GridSerializerTest.scala
@@ -1,29 +1,29 @@
-package com.github.debop4s.experiments.tests.serializers
-
-import com.github.debop4s.experiments.tests.AbstractExperimentTest
-import org.gridgain.grid.marshaller.optimized.GridOptimizedMarshaller
-
-/**
- * GridSerializerTest
- * Created by debop on 2014. 3. 23.
- */
-class GridSerializerTest extends AbstractExperimentTest {
-
-    test("GridOptimizedMarshaller test") {
-        val marshaller = new GridOptimizedMarshaller()
-
-        val data = GridSampleClass("debop", 47, 176.6f)
-        val bytes = marshaller.marshal(data)
-        val converted = marshaller.unmarshal(bytes, classOf[GridSampleClass].getClassLoader).asInstanceOf[GridSampleClass]
-
-        assert(converted != null)
-        assert(converted == data)
-    }
-
-}
-
-case class GridSampleClass(name: String, age: Int, score: Float) {
-    def this() {
-        this(null, 0, 0f)
-    }
-}
+//package com.github.debop4s.experiments.tests.serializers
+//
+//import com.github.debop4s.experiments.tests.AbstractExperimentTest
+//import org.gridgain.grid.marshaller.optimized.GridOptimizedMarshaller
+//
+///**
+// * GridSerializerTest
+// * Created by debop on 2014. 3. 23.
+// */
+//class GridSerializerTest extends AbstractExperimentTest {
+//
+//    test("GridOptimizedMarshaller test") {
+//        val marshaller = new GridOptimizedMarshaller()
+//
+//        val data = GridSampleClass("debop", 47, 176.6f)
+//        val bytes = marshaller.marshal(data)
+//        val converted = marshaller.unmarshal(bytes, classOf[GridSampleClass].getClassLoader).asInstanceOf[GridSampleClass]
+//
+//        assert(converted != null)
+//        assert(converted == data)
+//    }
+//
+//}
+//
+//case class GridSampleClass(name: String, age: Int, score: Float) {
+//    def this() {
+//        this(null, 0, 0f)
+//    }
+//}
