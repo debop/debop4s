1eb9a47b6b2368afa540f866cf574a04c0795246
배성혁 <sunghyouk.bae@gmail.com>
2014-07-13 05:24:25 +0900

    -. 클래스 생성 시 초기화 코드가 추가되면, 새로운 클래스가 되어 버린다.

---
+++ debop4s-core/src/test/scala/debop4s/core/ClassFunSuite.scala
@@ -0,0 +1,26 @@
+package debop4s.core
+
+import org.scalatest.{Matchers, FunSuite}
+import org.slf4j.LoggerFactory
+
+/**
+ * ClassFunSuite
+ * @author sunghyouk.bae@gmail.com
+ */
+class ClassFunSuite extends FunSuite with Matchers {
+
+  private lazy val log = LoggerFactory.getLogger(getClass)
+
+  test("create instance") {
+    val dummy:Dummy = new Dummy {
+      id = 1
+      name = "debop"
+    }
+    println(s"Dummy class = ${dummy.getClass}")
+  }
+
+  class Dummy {
+    var id:Int = _
+    var name:String = _
+  }
+}
