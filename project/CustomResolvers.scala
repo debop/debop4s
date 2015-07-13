655b76d763939b37e6df49c5bc044938ab1f5119
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2015-07-13 14:33:22 +0900

    -. kesti nexus 추가.

--- project/CustomResolvers.scala
+++ project/CustomResolvers.scala
@@ -2,6 +2,7 @@ import sbt._
 
 object CustomResolvers {
 
+  val kestiNexus = "Kesti Nexus" at "nexus.kesti.co.kr/content/groups/public"
   val localNexus = "Local Nexus" at "http://127.0.0.1:8081/nexus/content/groups/public"
   val sonatypeSTArch = "scalaTools Archive" at "https://oss.sonatype.org/content/groups/scala-tools"
   val mavenOrgRepo = "Maven.Org Repository" at "http://repo1.maven.org/maven2/org"
@@ -11,6 +12,7 @@ object CustomResolvers {
 
   /** CustomResolvers */
   val customResolvers = Seq(
+    kestiNexus,
     localNexus,
     Resolver.mavenLocal,
     Resolver.sonatypeRepo("releases"),
